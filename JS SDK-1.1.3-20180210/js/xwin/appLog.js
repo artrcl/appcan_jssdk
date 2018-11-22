957f9ae9e3e9f031c4b50229999bd663bc5c8911
lsd <lai3122@qq.com>
2018-11-22 14:15:41 +0800

    appLog > appcan.ready

--- JS SDK-1.1.3-20180210/js/xwin/appLog.js
+++ JS SDK-1.1.3-20180210/js/xwin/appLog.js
@@ -7,7 +7,7 @@ var appLog = {
      */
     log: function (s) {
         if (!this._enabled) return;
-        this._output("log", Array.prototype.slice.apply(arguments));
+        this._send("log", Array.prototype.slice.apply(arguments));
     },
 
     /**
@@ -16,7 +16,7 @@ var appLog = {
      */
     warn: function (s) {
         if (!this._enabled) return;
-        this._output("warn", Array.prototype.slice.apply(arguments));
+        this._send("warn", Array.prototype.slice.apply(arguments));
     },
 
     /**
@@ -25,10 +25,10 @@ var appLog = {
      */
     error: function (s) {
         if (!this._enabled) return;
-        this._output("error", Array.prototype.slice.apply(arguments));
+        this._send("error", Array.prototype.slice.apply(arguments));
     },
 
-    _output: function (t, sa) {
+    _send: function (t, sa) {
         if (!this._enabled) return;
         var err;
         try {
@@ -52,8 +52,29 @@ var appLog = {
             if (ret !== "") ret = ret.substring(2);
         }
 
-        appcan.logs("[ " + err[1] + " line : " + err[2] + "," + err[3] + " " + t + " ] " + ret);
+        this._sendlog("[ " + err[1] + " line : " + err[2] + "," + err[3] + " " + t + " ] " + ret);
     },
+
+    _sendlog: function (s) {
+        try {
+            if (window.uexLog) {
+                uexLog.sendLog(s);
+            } else {
+                if (this._logs.length === 0) {
+                    appcan.ready(function () {
+                        var thiz = appLog;
+                        for (var i = 0; i < thiz._logs.length; i++) {
+                            uexLog.sendLog(thiz._logs[i]);
+                        }
+                        thiz._logs = [];
+                    });
+                }
+                this._logs.push(s);
+            }
+        } catch (e) {
+            //return e;
+        }
+    }, _logs: [],
 
     get _enabled() {
         return window.appConfig.appLogEnabled;
